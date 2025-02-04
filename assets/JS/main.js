d17875b36967ac5040068fb2e0a004f86730a713
AGD3SIGN <brandon.agdesign@gmail.com>
2025-02-04 14:39:32 -0600

    portfolio updates

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -16,3 +16,15 @@ if(navClose){
       navMenu.classList.remove('show-menu')
    })
 }
+
+/* ========== Toggle portfolio images ========== */
+function filterProjects(category) {
+   let items = document.querySelectorAll('.gallery-item');
+   items.forEach(item => {
+       if (category === 'all') {
+           item.classList.remove('hidden');
+       } else {
+           item.classList.toggle('hidden', !item.classList.contains(category));
+       }
+   });
+}
